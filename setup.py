8c75c1b537d709f1a87dccc21ed11808b77bde9b
Eric Dill <edill@bnl.gov>
2014-12-16 11:55:26 -0500

    MNT: Attempt to get enaml files included

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
-
+packages = find_packages()
+print('\n\npackages: {}\n\n'.format(packages))
 setup(
     name='auto-enaml',
     version='0.1.0',
@@ -9,7 +10,7 @@ setup(
     url='https://github.com/blink1073/auto-enaml',
     description='Utilities for auto generation of Enaml widgets',
     long_description=open('README.rst').read(),
-    packages=['auto_enaml'],
+    packages=packages,
     package_data={'auto_enaml': ['*.enaml'],
                   'auto_enaml.tests': ['*.enaml'],
                   'auto_enaml.widgets': ['*.enaml']},
